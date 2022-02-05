0971b0ec0568e65873f599921414514b33464023
AmirAce <aamirjalali8@gmail.com>
2022-02-04 22:58:48 -0500

    Feature Update
    added toDollars

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println(Enter your Currency);
+			System.out.println("Enter your Currency");
+			Scanner scan = new Scanner(System.in);
+			int toman = scan.nextInt();
+			System.out.println(toDollars(toman));
 			
+	}
+	public static int toDollars(int toman) {
+		int Dollars = toman/3000;
+		return Dollars;
 	}
 
 }
